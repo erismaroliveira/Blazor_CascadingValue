5be53de34938b4fd8e676b9e935b88c2c12a47d9
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-04-11 15:46:15 -0300

    Initial commit

---
+++ Blazor_CascadingValue/Utils/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor_CascadingValue.Utils
+{
+    public class Config
+    {
+        public string CorDeFundo { get; set; } = "yellow";
+        public string TamanhoFonte { get; set; } = "40px";
+    }
+}
